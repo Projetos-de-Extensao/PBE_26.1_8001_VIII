ddd5a8140ee886c5d34159d3401af2108f02a23e
Pedrozcps <202503076936@alunos.ibmec.edu.br>
2026-06-01 14:41:43 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import (
+    Usuario,
+    Estudante,
+    Professor,
+    Coordenador,
+    EmpresaParceira,
+    SolicitacaoEstagio,
+    Documento,
+    Pendencia
+)
 
+admin.site.register(Usuario)
+admin.site.register(Estudante)
+admin.site.register(Professor)
+admin.site.register(Coordenador)
+admin.site.register(EmpresaParceira)
+admin.site.register(SolicitacaoEstagio)
+admin.site.register(Documento)
+admin.site.register(Pendencia)
 # Register your models here.
