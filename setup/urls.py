016e8509e0336af1a8907f60cbe03a3b31fbfd59
Pedrozcps <202503076936@alunos.ibmec.edu.br>
2026-06-01 14:43:54 -0300

    resolve conflitos de merge e adiciona rotas de documentação
    Removed old URL configuration comments and added new API schema and documentation paths.

--- setup/urls.py
+++ setup/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-<<<<<<< HEAD
-
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from core.views import (
     home,
@@ -33,12 +17,7 @@ from core.views import (
     PendenciaViewSet,
 )
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 router = DefaultRouter()
-
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'estudantes', EstudanteViewSet)
 router.register(r'professores', ProfessorViewSet)
@@ -48,20 +27,13 @@ router.register(r'solicitacoes', SolicitacaoEstagioViewSet)
 router.register(r'documentos', DocumentoViewSet)
 router.register(r'pendencias', PendenciaViewSet)
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
->>>>>>> 721beea9b8ef3518ddc1c3c7d237a09c592b88db
